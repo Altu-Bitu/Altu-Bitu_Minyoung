af48d5ae71e9e8aca563742e0650519c5f66cbf3
min0e <71228299+min0e@users.noreply.github.com>
2021-09-07 17:46:05 +0900

    Create BJ11651.cpp

---
+++ HW/0903/BJ11651.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int x;
+	int y;
+}P;
+
+int compare(const void* first, const void* second)
+{
+	P* a = (P*)first;
+	P* b = (P*)second;
+
+	if (a->y < b->y)
+		return -1;
+	else if (a->y > b->y)
+		return 1;
+	else
+	{
+		if (a->x < b->x)
+			return -1;
+		else
+			return 1;
+	}
+	return 0;
+}
+
+int main()
+{
+	int i, n;
+	P* list;
+
+	scanf("%d", &n);
+	list = (P*)malloc(n * sizeof(P));
+
+	for (i = 0; i < n; i++)
+	{
+		scanf(" %d %d", &list[i].x, &list[i].y);
+	}
+
+	qsort(list, n, sizeof(list[0]), compare);
+
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d %d\n", list[i].x, list[i].y);
+	}
+	return 0;
+}
